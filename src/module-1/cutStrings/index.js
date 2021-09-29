c69f559f9e405c791c5c9b449f81dc67f240475d
masterflo1512 <sakharovbig@gmail.com>
2021-09-29 21:21:51 +0300

    module1

--- src/module-1/cutStrings/index.js
+++ src/module-1/cutStrings/index.js
@@ -1,1 +1,15 @@
-export const cutStrings = (arr = []) => {};
+export function cutStrings (arr = []) {
+    
+    function getStringLength(str) {
+        return str.length;
+    }
+
+    function cutOneString(str) {
+        return str.slice(0, shortestLength);
+    }
+
+    const stringLengths = arr.map(getStringLength)
+    const shortestLength = Math.min(...stringLengths)
+
+    return arr.map(cutOneString)
+}
--- src/module-1/debounce/index.js
+++ src/module-1/debounce/index.js
@@ -1,5 +1,15 @@
 export const debounce = (fn, delay = 0) => {
+    let timeout;
 
+    return (...rest) => {
+        if (timeout !== undefined) {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(() => {
+            fn(...rest);
+        }, delay);
+    };
 }
 
 
