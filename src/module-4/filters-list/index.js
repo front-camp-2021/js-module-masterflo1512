556d0c2233fd4ea5ed3ac8b3e549bd32431519f5
masterflo1512 <sakharovbig@gmail.com>
2021-10-07 14:27:03 +0300

    main changes

--- src/module-4/filters-list/index.js
+++ src/module-4/filters-list/index.js
@@ -1,11 +1,47 @@
-export default class FiltersList {
-  constructor ({
-     title = '',
-     list = []
-  } = {}) {
-    this.title = title;
-    this.list = list;
+  export default class FiltersList {
+    constructor ({
+      title = '',
+      list = []
+    } = {}) {
+      this.title = title;
+      this.list = list;
+      this.render();
+    }
 
-    // ... your logic
+    getTitleTemplate(){
+      return `<h3 class="filter-title">${this.title}</h3>`;
+    }
+    
+    getFilterItemTemplate({value = 0, title = '', checked = false}) {
+      return `<div class="form-checkbox-category">
+        <label>
+            <input type="checkbox" name="category" ${checked ? 'checked' : ''}/>
+            ${title}
+        </label>
+        <span class="amount">${value}</span>
+      </div>`;
+    }
+    
+    getFilterListTemplate() {
+      return this.list.map((item) => {
+        return this.getFilterItemTemplate(item);
+      }).join(''); 
+    }
+
+    render() {
+      const wrapper = document.createElement('div');
+      wrapper.innerHTML = this.getTitleTemplate() + this.getFilterListTemplate();
+      this.element = wrapper;
+    }
+    
+    remove() {
+      if (this.element) {
+          this.element.remove();
+      }
+    }
+
+    destroy() {
+      this.remove();
+      this.element = null;
+    }
   }
-}
